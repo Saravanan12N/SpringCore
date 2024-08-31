1054c1fe4088ffe9310a1d5dfee3edf807b7cfb7
Sarvanan N <nsaravanan1204@gmail.com>
2024-08-31 23:27:18 +0530

    Properties added

--- src/main/java/com/example/Learn/BubbleSortAlog.java
+++ src/main/java/com/example/Learn/BubbleSortAlog.java
@@ -1,5 +1,9 @@
 package com.example.Learn;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class BubbleSortAlog implements  SortAlgorithm{
 
     public int[] sort(int[] numbers){
